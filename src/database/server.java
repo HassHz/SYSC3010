2574f3e77417cb3d7f38325b863c723f40131e09
HassHz <hassaanhafeez@hotmail.com>
2017-03-27 23:29:09 -0400

    Fix formatting in server.java file

--- src/database/server.java
+++ src/database/server.java
@@ -16,62 +16,62 @@ public class server {
 
     public void run(int port) {
     	try {
-			database database = new database("Carleton", fileLocation);
+            database database = new database("Carleton", fileLocation);
 
-			@SuppressWarnings("resource")
-			DatagramSocket serverSocket = new DatagramSocket(port);
+            @SuppressWarnings("resource")
+            DatagramSocket serverSocket = new DatagramSocket(port);
 
-			byte[] receiveData = new byte[256];
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			if (debugging) {System.out.printf("Listening on udp:%s:%d%n",InetAddress.getLocalHost().getHostAddress(), port);}
+            byte[] receiveData = new byte[256];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            if (debugging) {System.out.printf("Listening on udp:%s:%d%n",InetAddress.getLocalHost().getHostAddress(), port);}
 
-        	while(true) {
-				serverSocket.receive(receivePacket);
-				String dataReceived = new String(receivePacket.getData(), 0, receivePacket.getLength());
-				if (debugging) {System.out.println("RECEIVED: " + dataReceived);}
-				InetAddress IPAddress = receivePacket.getAddress();
+            while(true) {
+                serverSocket.receive(receivePacket);
+                String dataReceived = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                if (debugging) {System.out.println("RECEIVED: " + dataReceived);}
+                InetAddress IPAddress = receivePacket.getAddress();
 
-				if (dataReceived.contains("notification") && dataReceived.contains("fire")) {
+                if (dataReceived.contains("notification") && dataReceived.contains("fire")) {
 
-					String sendString = "received";
-                	byte[] sendData = sendString.getBytes("UTF-8");
-                	DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receivePacket.getPort());
-                	serverSocket.send(sendPacket);
+                    String sendString = "received";
+                    byte[] sendData = sendString.getBytes("UTF-8");
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receivePacket.getPort());
+                    serverSocket.send(sendPacket);
 
-				} else if (dataReceived.contains("add") || dataReceived.contains("remove") || dataReceived.contains("get")) {
+                } else if (dataReceived.contains("add") || dataReceived.contains("remove") || dataReceived.contains("get")) {
 
-					if (dataReceived.contains("get:")){
+                    if (dataReceived.contains("get:")){
 
-						String[] entryData = dataReceived.split(":");
-	            		if (entryData.length > 1) {
-	                    	String member = database.getMemberById(fileLocation, entryData[1]);
-							if (member != null) {
-	                    		byte[] sendData = member.getBytes("UTF-8");
-	                    		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receivePacket.getPort());
-	                    		serverSocket.send(sendPacket);
-							}
-						}
+                        String[] entryData = dataReceived.split(":");
+                        if (entryData.length > 1) {
+                            String member = database.getMemberById(fileLocation, entryData[1]);
+                            if (member != null) {
+                                byte[] sendData = member.getBytes("UTF-8");
+                                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, receivePacket.getPort());
+                                serverSocket.send(sendPacket);
+                            }
+                        }
 
-	            	} else if (dataReceived.contains("add:")) {
+                    } else if (dataReceived.contains("add:")) {
 
-						String[] entryData = dataReceived.split(":");
-	            		if (entryData.length > 3) {
-	            			database.addMember(fileLocation, entryData[1], entryData[2], entryData[3]);
-						}
+                        String[] entryData = dataReceived.split(":");
+                        if (entryData.length > 3) {
+                            database.addMember(fileLocation, entryData[1], entryData[2], entryData[3]);
+                        }
 
-					} else if (dataReceived.contains("remove:")) {
+                    } else if (dataReceived.contains("remove:")) {
 
-	            		String[] entryData = dataReceived.split(":");
-	            		if (entryData.length > 1) {
-	            			database.removeMember(fileLocation, entryData[1]);
-						}
+                        String[] entryData = dataReceived.split(":");
+                        if (entryData.length > 1) {
+                            database.removeMember(fileLocation, entryData[1]);
+                        }
 
-					}
+                    }
 
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
